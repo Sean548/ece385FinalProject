4ebdd3bf7d52050b8721253ce7f64eef9725e759
Peter-Hevrdejs <50895261+Peter-Hevrdejs@users.noreply.github.com>
2019-11-27 14:08:48 -0600

    Update Mario.sv
    changed dpad movement

--- Mario.sv
+++ Mario.sv
@@ -18,7 +18,7 @@ module  Mario ( input         Clk,                // 50 MHz clock
                              Reset,              // Active-high reset signal
                              frame_clk,          // The clock indicating a new frame (~60Hz)
                input [9:0]   DrawX, DrawY,       // Current pixel coordinates
-					input [3:0]  keymove,
+	       				input [1:0]  keymove,
 					input 	     keypress_B,
 	       				input        keypress_A,
 					input        pause,
