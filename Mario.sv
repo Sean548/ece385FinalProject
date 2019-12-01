e8be7116f051b8255e4d3ae57a2311303eae66f7
Sean548 <43865127+Sean548@users.noreply.github.com>
2019-12-01 16:58:27 -0600

    Update Mario.sv
    basic movement

--- Mario.sv
+++ Mario.sv
@@ -16,25 +16,28 @@
 
 module  Mario ( input         Clk,                // 50 MHz clock
                              Reset,              // Active-high reset signal
-                             frame_clk,          // The clock indicating a new frame (~60Hz)
-               input [9:0]   DrawX, DrawY,       // Current pixel coordinates
-	       input [1:0]   keymove,            // right x01, left x02, down x03 , up x04
-	       input	     keypress_B,         // Run (allows for hold. high is 1, low is 0)
-	       input         keypress_A,	 // Jump (resets after every press aka no hold)
+									  frame_clk,          // The clock indicating a new frame (~60Hz)
+          input [9:0]   DrawX, DrawY,       // Current pixel coordinates
+	       input [1:0]   keymove,            // right x01, left x02, down x03
+	       input	      keypress_B,         // Run (allows for hold. high is 1, low is 0)
+	       input         keypress_A,	        // Jump (resets after every press aka no hold)
 	       input         pause,              // Pauses all elements. high is one.
 	       input [1:0]  Size,		 //says what size mario is
                output logic Is_Mario             // Whether current pixel belongs to ball or background
               );
     
-    parameter [9:0] Ball_X_Center = 10'd320;  // Center position on the X axis
-    parameter [9:0] Ball_Y_Center = 10'd240;  // Center position on the Y axis
+    parameter [9:0] Ball_X_Center = 10'd32;  // Center position on the X axis
+    parameter [9:0] Ball_Y_Center = 10'd591  // Center position on the Y axis
     parameter [9:0] Ball_X_Min = 10'd0;       // Leftmost point on the X axis
     parameter [9:0] Ball_X_Max = 10'd639;     // Rightmost point on the X axis
     parameter [9:0] Ball_Y_Min = 10'd0;       // Topmost point on the Y axis
     parameter [9:0] Ball_Y_Max = 10'd479;     // Bottommost point on the Y axis
     parameter [9:0] Ball_X_Step = 10'd1;      // Step size on the X axis
-    parameter [9:0] Ball_Y_Step = 10'd1;      // Step size on the Y axis
-    parameter [9:0] Ball_Size = 10'd4;        // Ball size
+    parameter [9:0] Ball_Y_Step = 10'd20;      // Step size on the Y axis
+    parameter [9:0] Ball_Size = 10'd32;        // Ball size
+	 parameter [9:0] Ball_Y_Max = 10'd575;
+	 
+	 //need to update ball size for different marios
     
     logic [9:0] Ball_X_Pos, Ball_X_Motion, Ball_Y_Pos, Ball_Y_Motion;
     logic [9:0] Ball_X_Pos_in, Ball_X_Motion_in, Ball_Y_Pos_in, Ball_Y_Motion_in;
@@ -73,7 +76,7 @@ module  Mario ( input         Clk,                // 50 MHz clock
         Ball_X_Pos_in = Ball_X_Pos;
         Ball_Y_Pos_in = Ball_Y_Pos;
         Ball_X_Motion_in = Ball_X_Motion;
-        Ball_Y_Motion_in = Ball_Y_Motion;
+        Ball_Y_Motion_in = Ball_Y_Motion +1'b1;
         
         // Update position and motion only at rising edge of frame clock
         if (frame_clk_rising_edge)
@@ -82,49 +85,88 @@ module  Mario ( input         Clk,                // 50 MHz clock
             //   both sides of the operator as UNSIGNED numbers.
             // e.g. Ball_Y_Pos - Ball_Size <= Ball_Y_Min 
             // If Ball_Y_Pos is 0, then Ball_Y_Pos - Ball_Size will not be -4, but rather a large positive number.
-//				check = check;
-				if(keycode == 8'h1a) //check for up
+				//check = check;
+				
+				
+				//left and right movement with sprint but no acceleration yet
+				if(keymove==3'b000)
+					Ball_X_Motion_in=0;
+				if(keymove==3'b001)
+				
 				begin
-					Ball_Y_Motion_in = ~(Ball_Y_Step)+ 1'b1;
-					Ball_X_Motion_in= 0;
-				end
-				else if(keycode == 8'h16) //check for down
+					if(keypress_B==1'b0)
+						Ball_X_Motion_in=Ball_X_Step;
+					if(keypress_B==1'b1)
+						Ball_X_Motion_in=Ball_X_Step+1'b1;
+				if(keymove==3'b010)
 				begin
-					Ball_Y_Motion_in = (Ball_Y_Step);
-					Ball_X_Motion_in= 0;
+					if(keypress_B==1'b0)
+						Ball_X_Motion_in=~(Ball_X_Step)+ 1'b1;
+					if(keypress_B==1'b1)
+						Ball_X_Motion_in=~(Ball_X_Step);
 				end
-				else if(keycode == 8'h4) //check for left
+					
+				
+				
+				//jump with acceleration
+				Ball_Y_Motion_in = Ball_Y_Motion + 1;
+				
+				
+				if(keypress_A==1'b1&&Ball_Y_Motion==0)
 				begin
-					Ball_Y_Motion_in = 0;
-					Ball_X_Motion_in= ~(Ball_X_Step)+ 1'b1;
+					if(Ball_Y_Pos + Ball_Y_Size >= Ball_Y_Center)
+						Ball_Y_Motion_in=~(Ball_Y_Step)+1'b1;
 				end
-				else if(keycode == 8'h7) //check for right
-				begin
-					Ball_Y_Motion_in = 0;
-					Ball_X_Motion_in= Ball_X_Step;
-				end
-            if( Ball_Y_Pos + Ball_Size >= Ball_Y_Max )  // Ball is at the bottom edge, BOUNCE!
-				begin
-                Ball_Y_Motion_in = (~(Ball_Y_Step) + 1'b1);  // 2's complement.  
-					 Ball_X_Motion_in = 0;
-				end
-            else if ( Ball_Y_Pos <= Ball_Y_Min + Ball_Size )  // Ball is at the top edge, BOUNCE!
-				begin
-                Ball_Y_Motion_in = Ball_Y_Step;
-					 Ball_X_Motion_in = 0;
-				end
-				else if( Ball_X_Pos + Ball_Size >= Ball_X_Max )  // Ball is at the right edge, BOUNCE!
-				begin
-                Ball_X_Motion_in = (~(Ball_X_Step) + 1'b1);  // 2's complement.  
-					 Ball_Y_Motion_in = 0;
-				end
-				else if ( Ball_X_Pos <= Ball_X_Min + Ball_Size )  // Ball is at the left edge, BOUNCE!
-				begin
-                Ball_X_Motion_in = Ball_X_Step;
-					 Ball_Y_Motion_in = 0;
-				end
-            // TODO: Add other boundary detections and handle keypress here.
-        
+				else if((Ball_Y_Pos + Ball_Y_Motion_+Ball_Size) > Ball_Y_Max)
+					Ball_Y_Pos_in = Ball_Y_Max-Ball_Size;
+				else
+					Ball_Y_Pos_in = Ball_Y_Pos + Ball_Y_Motion;
+				
+				
+
+//				
+//				if(keycode == 8'h1a) //check for up
+//				begin
+//					Ball_Y_Motion_in = ~(Ball_Y_Step)+ 1'b1;
+//					Ball_X_Motion_in= 0;
+//				end
+//				else if(keycode == 8'h16) //check for down
+//				begin
+//					Ball_Y_Motion_in = (Ball_Y_Step);
+//					Ball_X_Motion_in= 0;
+//				end
+//				else if(keycode == 8'h4) //check for left
+//				begin
+//					Ball_Y_Motion_in = 0;
+//					Ball_X_Motion_in= ~(Ball_X_Step)+ 1'b1;
+//				end
+//				else if(keycode == 8'h7) //check for right
+//				begin
+//					Ball_Y_Motion_in = 0;
+//					Ball_X_Motion_in= Ball_X_Step;
+//				end
+//            if( Ball_Y_Pos + Ball_Size >= Ball_Y_Max )  // Ball is at the bottom edge, BOUNCE!
+//				begin
+//                Ball_Y_Motion_in = (~(Ball_Y_Step) + 1'b1);  // 2's complement.  
+//					 Ball_X_Motion_in = 0;
+//				end
+//            else if ( Ball_Y_Pos <= Ball_Y_Min + Ball_Size )  // Ball is at the top edge, BOUNCE!
+//				begin
+//                Ball_Y_Motion_in = Ball_Y_Step;
+//					 Ball_X_Motion_in = 0;
+//				end
+//				else if( Ball_X_Pos + Ball_Size >= Ball_X_Max )  // Ball is at the right edge, BOUNCE!
+//				begin
+//                Ball_X_Motion_in = (~(Ball_X_Step) + 1'b1);  // 2's complement.  
+//					 Ball_Y_Motion_in = 0;
+//				end
+//				else if ( Ball_X_Pos <= Ball_X_Min + Ball_Size )  // Ball is at the left edge, BOUNCE!
+//				begin
+//                Ball_X_Motion_in = Ball_X_Step;
+//					 Ball_Y_Motion_in = 0;
+//				end
+//            // TODO: Add other boundary detections and handle keypress here.
+//        
         
             // Update the ball's position with its motion
             Ball_X_Pos_in = Ball_X_Pos + Ball_X_Motion;
