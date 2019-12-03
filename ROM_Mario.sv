93a82f7dfdcbab58fe1dd0d12ac1471ab7c0e272
Peter-Hevrdejs <50895261+Peter-Hevrdejs@users.noreply.github.com>
2019-12-03 08:44:13 -0600

    Update ROM_Mario.sv
    edited

--- ROM_Mario.sv
+++ ROM_Mario.sv
@@ -1,6 +1,6 @@
-module ROM_Mario( input [18:0] READ_ADDR,
-						input WE, Clk,
-						output logic [4:0] DATA_OUT
+module ROM_Mario( input [12:0] READ_ADDR,
+						input Clk,
+						output logic [1:0] DATA_OUT
 					  );
 
 logic [1:0] mem [0:6911]; //logic[<Number of Bits per address - 1> : 0] mem [0 : <Number of Addresses - 1>];
